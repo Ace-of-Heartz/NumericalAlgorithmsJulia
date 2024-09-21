53fa1fa7d4b96266a009d781f8f3b423e05d5b7c
Ferenci Ákos Sándor <synthetace@gmail.com>
2024-09-21 23:35:57 +0200

    Nix  and direnv setup

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{ 
+  description = "Julia project flake";
+
+  inputs = {
+    nixgl.url = "github:nix-community/nixGL";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs = { self, nixgl, nixpkgs }:
+    let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system; 
+        overlays = [nixgl.overlay];
+      };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nixgl.nixGLIntel
+        ];
+
+        packages = with pkgs; [
+          julia-bin
+          gr-framework
+          stdenv.cc.cc.lib qt5.qtbase qt5Full libGL
+          glxinfo
+          glfw
+        ];
+
+        shellHook = ''
+          nixGLIntel julia
+        '';
+      };
+      
+    };
+}
