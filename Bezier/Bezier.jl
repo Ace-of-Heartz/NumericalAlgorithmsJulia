6ad3b9c76cafd8e32bc19eca331b93a2c400d191
Ace-of-Heartz <synthetace@gmail.com>
2024-10-15 08:02:22 +0200

    Added Bezier file

---
+++ Bezier/Bezier.jl
@@ -0,0 +1,17 @@
+using GLMakie 
+
+function plot_bezier2d(
+    xs :: Array{<:Number}
+)
+    t = Observable(0)
+
+    fig = Figure()
+    record(fig[1,1],t,
+    
+    )
+
+    for i = 1:n 
+        b 
+    end
+
+end
