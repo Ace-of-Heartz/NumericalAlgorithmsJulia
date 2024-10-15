f39c5e3f5aad52cb312ec7f88b09ede313addf67
Ace-of-Heartz <synthetace@gmail.com>
2024-10-15 08:15:14 +0200

    Updated Bezier.jl

--- Bezier/Bezier.jl
+++ Bezier/Bezier.jl
@@ -5,13 +5,24 @@ function plot_bezier2d(
 )
     t = Observable(0)
 
-    fig = Figure()
-    record(fig[1,1],t,
-    
-    )
+    frames = 1:0.2:10
 
-    for i = 1:n 
-        b 
-    end
+    fig = Figure()
+    record(fig[1,1],frames,
+        framerate = 24) do t
+            bs_old = xs 
+            bs_new = []
+            for j = 1:n 
+                for i = 1:n-j 
+                    push!(bs_new,bs_old[i] * (1 - t) + bs_old[i + 1] * t) 
+                end
+                bs_old = bs_new
+                bs_new = []
+            end
+        end 
+
+
+
+
 
 end
