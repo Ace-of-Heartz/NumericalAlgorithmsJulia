ec85be18d2e6ca04fbe9b3f915b2085ade5334b9
Ace-of-Heartz <synthetace@gmail.com>
2024-10-16 19:59:45 +0200

    Updated Bezier.jl
    Implemented De Casteljau algorithm

--- Bezier/Bezier.jl
+++ Bezier/Bezier.jl
@@ -1,28 +1,43 @@
 using GLMakie 
 
-function plot_bezier2d(
-    xs :: Array{<:Number}
-)
-    t = Observable(0)
+function de_casteljau(t,xs)
+    n = length(xs)
+    
+    bs_old = xs
+    bs_new = []
+    for j = 1:n
+        for i = 1:n-j
+            push!(bs_new, bs_old[i+1] .* (1 - t) + bs_old[ini] .* t)
+        end
 
-    frames = 1:0.2:10
+        if length(bs_new) == 0
+            break
+        end
+        print("BS Old: $bs_old\nBS New: $bs_new\n")
+        bs_old = bs_new
+        bs_new = []
 
-    fig = Figure()
-    record(fig[1,1],frames,
-        framerate = 24) do t
-            bs_old = xs 
-            bs_new = []
-            for j = 1:n 
-                for i = 1:n-j 
-                    push!(bs_new,bs_old[i] * (1 - t) + bs_old[i + 1] * t) 
-                end
-                bs_old = bs_new
-                bs_new = []
-            end
-        end 
-
-
-
-
-
+    end
+    
+    return Point2f(bs_old[1])
 end
+
+function plot_bezier2d(
+    xs 
+)
+    ps = Observable(de_casteljau(0,xs));
+
+    frames = 0:0.2:10
+    
+    fig = Figure()
+    ax = Axis(fig[1,1])
+
+    lines!(fig[1,1],ps)
+    
+    record(fig,"Bezier.gif",frames;
+        framerate = 24) do k
+            b = de_casteljau(k,xs)
+            ps[] = push!(ps[],b)
+        end 
+end
+
