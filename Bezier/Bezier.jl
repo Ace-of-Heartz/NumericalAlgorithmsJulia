95b5287cf8e06dbd9164b9c9bca5cab32a3fb4a3
Ace-of-Heartz <synthetace@gmail.com>
2024-10-28 00:58:09 +0100

    Updated Bezier.jl
    Debugged De-Casteljau algorithm
    Added additional functionalities

--- Bezier/Bezier.jl
+++ Bezier/Bezier.jl
@@ -1,4 +1,3 @@
-using GLMakie 
 
 function de_casteljau(t,xs)
     n = length(xs)
@@ -7,13 +6,12 @@ function de_casteljau(t,xs)
     bs_new = []
     for j = 1:n
         for i = 1:n-j
-            push!(bs_new, bs_old[i+1] .* (1 - t) + bs_old[ini] .* t)
+            push!(bs_new, bs_old[i] .* (1 - t) + bs_old[i+1] .* t)
         end
 
         if length(bs_new) == 0
             break
         end
-        print("BS Old: $bs_old\nBS New: $bs_new\n")
         bs_old = bs_new
         bs_new = []
 
@@ -22,22 +20,56 @@ function de_casteljau(t,xs)
     return Point2f(bs_old[1])
 end
 
-function plot_bezier2d(
-    xs 
-)
-    ps = Observable(de_casteljau(0,xs));
+function de_casteljau_mtx(t,xs)
+    n = length(xs)
 
-    frames = 0:0.2:10
-    
-    fig = Figure()
-    ax = Axis(fig[1,1])
+    mtx = ones(n,n) .* xs
 
-    lines!(fig[1,1],ps)
-    
-    record(fig,"Bezier.gif",frames;
-        framerate = 24) do k
-            b = de_casteljau(k,xs)
-            ps[] = push!(ps[],b)
-        end 
+    for j = 2:n 
+        for i = j:n 
+            mtx[i,j] = mtx[(i-1),j-1] .* (1-t) + mtx[i,j-1] .* t  
+        end
+    end
+
+    return mtx
+
 end
+
+function constr_sub_curve(
+    xs,
+    t :: Number 
+    ) 
+    
+    ns = 1:1:length(xs)
+    new_xs = map(n -> de_casteljau(t,xs[1:n]),ns)
+
+    return new_xs
+end
+
+function elevate(xs)
+    n = length(xs)
+
+    new_xs = ones(n + 1)
+
+    new_xs[1] = xs[0]
+    new_xs[n+1] = xs[n]
+    
+    for k = 2:n
+        new_xs[k] = k/(n+1) *  xs[k-1] + (1 - k/(n+1)) * xs[k]
+    end
+
+    return new_xs
+end
+
+function bezier2d(
+    xs,
+    t_max = 1,
+    t_step = 0.01
+) 
+    frames = 0:t_step:t_max    
+    ts  = map(t -> de_casteljau(t,xs),frames)
+    return ts
+end
+
+
 
