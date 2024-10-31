986b673f8f414bf15fb60079d6e16c2919ad6441
Ace-of-Heartz <synthetace@gmail.com>
2024-11-01 00:52:58 +0100

    Cleaned up Chase.jl

--- ParametricCurves/Chase/Chase.jl
+++ ParametricCurves/Chase/Chase.jl
@@ -17,15 +17,10 @@ xs = @. X(trange); ys = @. Y(trange)
 uxs = sol[1,1,:]
 uys = sol[2,1,:]
 
-
-
 framerate = 24
 
 fig = Figure()
 axis = Axis(fig[1, 1])
-
-
-
 
 limits!(axis,
     minimum(vcat(xs,uxs)),
@@ -35,30 +30,5 @@ limits!(axis,
     )
 
 animate(fig[1,1],X.(sol.t),Y.(sol.t),uxs,uys,6)
-
-# points1 = Observable(Point2f[(xs[1],ys[1])])
-# points2 = Observable(Point2f[(uxs[1],uys[1])])
-
-# lines!(fig[1,1],points1,color = :red)
-
-# lines!(fig[1,1],points2, color = :blue) 
-
-# frames = 1:length(uxs);
-
-# print(length(uxs))
-
-# record(fig,"chase_anim.gif",frames;
-#     framerate = framerate) do k
-#         t = sol.t[k]
-
-#         new_point1 = Point2f(X(t),Y(t))
-#         new_point2 = Point2f(uxs[k],uys[k])
-#         points1[] = push!(points1[], new_point1)
-#         points2[] = push!(points2[], new_point2)
-#     end
-
-
-
-
 
 return fig
