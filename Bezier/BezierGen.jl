5c50071d1e906a54f1923600e82b81bc00a55eba
Ace-of-Heartz <synthetace@gmail.com>
2024-11-06 20:05:03 +0100

    Added BezierGen.jl

---
+++ Bezier/BezierGen.jl
@@ -0,0 +1,25 @@
+include("Bezier.jl")
+
+function gen_bezier(
+    n :: Number,
+    noise :: AbstractVector{<:Number}
+)
+    linfit
+end
+
+function gen_bezier(
+    n :: Number, 
+    xN :: Number, yN :: Number
+)
+    
+end
+
+function gen_bezier(
+    n :: Number,
+    xN :: Number, yN :: Number, zN :: Number
+)
+
+end
+
+function gen_bezier_surf()
+end
