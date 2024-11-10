c7e8eccc3915af072ab9cd9ff2c2cdbad433dda0
Ace-of-Heartz <synthetace@gmail.com>
2024-11-11 00:12:34 +0100

    Updated functions responsible for generating points
    Added more types and abstraction

--- Utils/Points.jl
+++ Utils/Points.jl
@@ -1,23 +1,39 @@
 function get_points(
     n :: Number,
-    dim :: Number = 2,
-    distance :: AbstractArray{<:Number} = [0.1,0.1],
-    limit :: AbstractArray{<:Tuple{<:Number,<:Number} } = [(0,1),(0,1),(0,1)],
+    f :: Function,
+    distance :: Number = 0.1,
+    limit :: Tuple{<:Number,<:Number} = (0,1),
     )
 
-    ps = map(range(0,n * distance,n),distance)
+    xs = range(limit[1],min(n * distance,limit[2]),n)
+    ys = f.(xs)
 
-
+    return (xs,ys)
 end
 
 function get_points(
     n :: Number,
+    f :: Function,
+    g :: Function,
+    distance :: Number = 0.1,
+    limit :: Tuple{<:Number,<:Number} = (0,1),
+    )
+
+    xs = range(limit[1],min(n * distance,limit[2]),n)
+    ys = f.(xs)
+    zs = g.(xs,ys)
+
+
+    return (xs,ys)
+end
+
+function get_surface_points(
+    n :: AbstractArray{<:Number},
     f::Function,
-    dim :: Number = 2, 
-    distance :: AbstractArray{<:Number} = [0.1,0.1],
-    limit :: AbstractArray{<:Tuple{<:Number,<:Number} } = [(0,1),(0,1)],
+    distances :: AbstractArray{<:Number} = [0.1,0.1],
+    limits :: AbstractArray{<:Tuple{<:Number,<:Number} } = [(0,1),(0,1)],
 )
-    (xs,ys) = map(i -> range(limit[i][1],min(n * distance[i],limit[i][2]),n),eachindex(distance))
+    (xs,ys) = map(i -> range(limits[i][1],min(n[i] * distances[i],limits[i][2]),n[i]),eachindex(distances))
 
     xs = collect(xs)
     ys = collect(ys)
