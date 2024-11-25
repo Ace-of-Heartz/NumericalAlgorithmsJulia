ad602743864bc3cdc1071d860803ddd80da12ba1
Ace-of-Heartz <synthetace@gmail.com>
2024-11-25 11:53:03 +0100

    Changed function signatures in Points.jl
    Added option to get points using a starting point and a stepsize~distance
    Added option to get points uniequivalently distanced from each other in given interval

--- Utils/Points.jl
+++ Utils/Points.jl
@@ -1,11 +1,11 @@
 function get_points(
     n :: Number,
     f :: Function,
+    start :: Number = 0.0,
     distance :: Number = 0.1,
-    limit :: Tuple{<:Number,<:Number} = (0,1),
     )
 
-    xs = range(limit[1],min(n * distance,limit[2]),n)
+    xs = range(start,n * distance, length = n)
     ys = f.(xs)
 
     return (xs,ys)
@@ -14,12 +14,40 @@ end
 function get_points(
     n :: Number,
     f :: Function,
-    g :: Function,
-    distance :: Number = 0.1,
     limit :: Tuple{<:Number,<:Number} = (0,1),
     )
 
-    xs = range(limit[1],min(n * distance,limit[2]),n)
+    xs = range(limit[1],limit[2], length = n)
+    ys = f.(xs)
+
+    return (xs,ys)
+end
+
+
+function get_points(
+    n :: Number,
+    f :: Function,
+    g :: Function,
+    start :: Number = 0.0,
+    distance :: Number = 0.1,
+    )
+
+    xs = range(start,n * distance, length = n)
+    ys = f.(xs)
+    zs = g.(xs,ys)
+
+
+    return (xs,ys)
+end
+
+function get_points(
+    n :: Number,
+    f :: Function,
+    g :: Function,
+    limit :: Tuple{<:Number,<:Number} = (0,1),
+    )
+
+    xs = range(limit[1],limit[2], length = n)
     ys = f.(xs)
     zs = g.(xs,ys)
 
@@ -29,11 +57,26 @@ end
 
 function get_surface_points(
     n :: AbstractArray{<:Number},
-    f::Function,
+    f :: Function,
+    starts :: AbstractArray{<:Number} = [0.0,0.0],
     distances :: AbstractArray{<:Number} = [0.1,0.1],
+    
+)
+    (xs,ys) = map(i -> range(starts[i],n[i] * distances[i],length = n[i]),eachindex(distances))
+
+    xs = collect(xs)
+    ys = collect(ys)
+    zs = [f(X,Y) for X ∈ xs, Y ∈ ys]
+
+    return (xs,ys,zs)
+end
+
+function get_surface_points(
+    n :: AbstractArray{<:Number},
+    f::Function,
     limits :: AbstractArray{<:Tuple{<:Number,<:Number} } = [(0,1),(0,1)],
 )
-    (xs,ys) = map(i -> range(limits[i][1],min(n[i] * distances[i],limits[i][2]),n[i]),eachindex(distances))
+    (xs,ys) = map(i -> range(limits[i][1],limits[i][2],length = n[i]),eachindex(distances))
 
     xs = collect(xs)
     ys = collect(ys)
