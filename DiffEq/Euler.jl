46e789c003169ac5e90299a54b6b4fd7a29cfd48
Ace-of-Heartz <synthetace@gmail.com>
2024-11-25 21:02:21 +0100

    Updated Euler.jl
    Removed `euler()`

--- DiffEq/Euler.jl
+++ DiffEq/Euler.jl
@@ -1,25 +1,8 @@
 function euler(
-    (t0,tN) = (0,1),  #Initial Time
-    u0;
-    f,   #Set of functions
-    h = 0.01, 
-)
-    us = [];
-    ts = []
-    
-    for t in range(t0,tN,step = h)
-        push!(ts,t0 .+ n * h)
-        push!(us,)
-    end
-
-    return ts,us
-end
-
-function naive_euler(
     N :: Number,
     f :: Function,
+    u0 :: Number,
     (tL,tR) = (0,1),
-    u0 :: Number
 
 )
     h = (tR - tL) / N
@@ -29,10 +12,11 @@ function naive_euler(
 
     us[1] = u0
     ts[1] = tL
-    for n in 1:N+1
+    for n in 1:N-1
         ts[n+1] = ts[n] + h
         us[n+1] = us[n] + h * f(ts[n],us[n])
     end
 
     return (ts,us);
 end
+
