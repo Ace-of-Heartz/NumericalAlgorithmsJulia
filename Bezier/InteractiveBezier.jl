0c66ba802c8cab38ff9a9e0e794a0fcb432c1178
Ace-of-Heartz <synthetace@gmail.com>
2024-11-26 14:03:17 +0100

    Added starting file for interactive Bezier curves

---
+++ Bezier/InteractiveBezier.jl
@@ -0,0 +1,18 @@
+using GLMakie 
+using LinearAlgebra
+
+include("Bezier.jl")
+include("BezierGen.jl")
+
+function demo()
+    
+    fig = Figure()
+    axCurr = Axis(fig[1,1])
+    axPrev = Axis(fig[1,2])
+    while true
+        
+    end
+
+    
+
+end
