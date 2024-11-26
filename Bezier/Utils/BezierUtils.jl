8da7e4ec40396a55d0a9f151030f310ef6011fec
Ace-of-Heartz <synthetace@gmail.com>
2024-11-26 14:03:28 +0100

    Updated BezierUtils.jl

--- Bezier/Utils/BezierUtils.jl
+++ Bezier/Utils/BezierUtils.jl
@@ -1,3 +1,18 @@
 struct BezierCurve
+    __ps
+    
+    function insert_point(
+        p, 
+        idx :: Integer
+        )
+        insert!(__ps,idx,p)
+    end
+    
+    function get_points()
+        return __ps
+    end
+end
 
+struct BezierSurface
+    curves :: AbstractArray{<:BezierCurve}
 end
